2a6b7d014ff1f563954dbea303e7f879a6f5e27e
user <user@JBTC10103>
2018-08-23 15:04:35 +0900

    제02차시

---
+++ 자바기초실습/src/제02차시/AppMain.java
@@ -0,0 +1,14 @@
+package 제02차시;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		System.out.println("난main() 함수!");
+		System.out.println("자바 프로그램은 나로부터 시작해!");
+
+		new Class01();
+		System.out.println("자바프로그램들은 이제 끝!");
+
+	}
+
+}
---
+++ 자바기초실습/src/제02차시/Class01.java
@@ -0,0 +1,26 @@
+package 제02차시;
+
+public class Class01 {
+	int i = 0;
+
+	Class01() {
+		i += 3;
+		System.out.println("난 Class01 클래스의 생성자란다");
+		System.out.println("Class01 클래스의 모양으로 객체를 만든즉시 호출됐지");
+		System.out.println("난 i, 필드 또는맴버 변수라고도 하지. c언어의 전역 번수정도로 이해하자");
+		System.out.println("현재i:" + i);
+
+		doIt();
+
+	}
+	
+	void doIt() {
+		i+=5;
+		System.out.println("난 doIt 메소드란다");
+		System.out.println("내가필요할땐나를불러줘 언제든지달려갈께");
+		System.out.println("난 i, 뭐라고 부른다고? 필드 또는 맴버변수라니깐");
+		System.out.println("현재i:" + i);
+
+	}
+
+}
