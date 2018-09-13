09fb95337119c20b8db697c4b7071d0e01464917
user <user@JBTC10103>
2018-09-13 11:33:58 +0900

    10103

---
+++ 자바과제/src/자바종합실습과제/문제11.java
@@ -0,0 +1,19 @@
+package 자바종합실습과제;
+
+import java.util.Scanner;
+
+public class 문제11 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.useDelimiter("\r|\n|,");
+		int x1, x2, y1, y2, z1, z2;
+		System.out.print("원(x1,y1,z1):");
+		x1 = sc.nextInt();
+		y1 = sc.nextInt();
+		z1 = sc.nextInt();
+		sc.nextInt();
+
+	}
+
+}
