eb08332e66453083dc9ecf76b992f401e2d062a9
user <user@JBTC10103>
2018-09-27 15:05:32 +0900

    10103

--- 자바과제/src/자바종합실습과제/문제45.java
+++ 자바과제/src/자바종합실습과제/문제45.java
@@ -1,9 +1,32 @@
 package 자바종합실습과제;
 
+import java.util.Scanner;
+
 public class 문제45 {
 
 	public static void main(String[] args) {
-		String[][] arr=
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n][n];
+		int i, j;
+
+		for (i = 0; i < arr.length; i++)
+			for (j = 0; j < arr[i].length; j++)
+				arr[i][j] = 0;
+
+		for (i = 0; i < arr.length; i++)
+			for (j = 0; j < arr[i].length; j++)
+				if (j == 0 || i == j)
+					arr[i][j] = 1;
+				else if (i > j)
+					arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+
+		for (i = 0; i < arr.length; i++) {
+			for (j = 0; j < arr[i].length; j++)
+				if (arr[i][j] > 0)
+					System.out.printf("%3d ", arr[i][j]);
+			System.out.println();
+		}
 
 	}
 
